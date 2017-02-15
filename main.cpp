1cc742c23e89e1ba9c05e09c25851b237c4fea77
Varun-Sethu <varun.sethu1@education.nsw.gov.au>
2017-02-15 19:59:03 +1100

    victory

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <string>
 
 //global board variable
-std::string board[3][3] = {{"[ ]","[ ]","[ ]"},{"[ ]","[ ]","[ ]"},{"[ ]","[ ]","[ ]"}};
+std::string board[3][3] = {{"[ ]","[ ]","[X]"},
+                            {"[ ]","[X]","[ ]"},
+                            {"[X]","[X]","[ ]"}};
 bool victory = false;
 
 void printBoard() {
         for(int i = 0; i < 3; i++) {
-            std::cout << board[i][0];
+            std::cout << board[0][i];
         }
     std::cout << std::endl;
         for(int i = 0; i < 3; i++) {
-            std::cout << board[i][1];
+            std::cout << board[1][i];
         }
     std::cout << std::endl;
         for(int i = 0; i < 3; i++){
-            std::cout << board[i][2];
+            std::cout << board[2][i];
         }
     std::cout << std::endl << std::endl;
 }
@@ -27,25 +29,25 @@ std::string setPeice(int peice, std::string value) {
         board[0][0] = "["+value+"]";
         return value;
     }else if(peice == 2){
-        board[1][0] = "["+value+"]";
+        board[0][1] = "["+value+"]";
         return value;
     }else if(peice == 3){
-        board[2][0] = "["+value+"]";
+        board[0][2] = "["+value+"]";
         return value;
     }else if(peice == 4){
-        board[0][1] = "["+value+"]";
+        board[1][0] = "["+value+"]";
         return value;
     }else if(peice == 5){
         board[1][1] = "["+value+"]";
         return value;
     }else if(peice == 6){
-        board[2][1] = "["+value+"]";
+        board[1][2] = "["+value+"]";
         return value;
     }else if(peice == 7){
-        board[0][2] = "["+value+"]";
+        board[2][0] = "["+value+"]";
         return value;
     }else if(peice == 8){
-        board[1][2] = "["+value+"]";
+        board[2][1] = "["+value+"]";
         return value;
     }else if(peice == 9){
         board[2][2] = "["+value+"]";
@@ -65,28 +67,38 @@ void getUserInp() {
 
 bool evalVictory() {
     bool win = false;
-    for(int i = 0; i < 8; i++){
-        if(i < 3) {
-            std::string currentCell = board[i][0];
-                if(i == 1) {
-                    //condition 1
-                    int winTimer = 0;
-                    if(board[i++][0] == currentCell){
-                        winTimer++;
-                    }else if(board[i][2] == currentCell){
-                        winTimer++;
-                    }else if(board[i++][2] == currentCell){
-                        winTimer ++;
-                    }else {
-                        win = false;
-                    }
-                }
-        }
+    if (board[0][0] != "[ ]" && (board[0][0] == board[0][1] && board[0][0] == board[0][2] ||
+		board[0][0] == board[1][0] && board[0][0] == board[2][0] ||
+		board[0][0] == board[1][1] && board[0][0] == board[2][2])){
+
+        win = true;
+        return win;
     }
+
+    if (board[1][1] != "[ ]" && (board[1][1] == board[1][0] && board[1][1] == board[1][2] ||
+		board[1][1] == board[0][1] && board[1][1] == board[2][1] ||
+		board[1][1] == board[2][0] && board[1][1] == board[0][2])) {
+
+		win = true;
+        return win;
+    }
+
+    if (board[2][2] != "[ ]" && (board[2][2] == board[0][2] && board[2][2] == board[1][2] ||
+		board[2][2] == board[2][0] && board[2][2] == board[2][1])){
+
+		win = true;
+        return win;
+    }
+}
+
+void ai(std::string boardState) {
+
 }
 
 int main(void)
 {
+    bool val = evalVictory();
+    std::cout << val << std::endl;
     printBoard();
     getUserInp();
     return 0;
